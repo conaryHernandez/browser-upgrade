d15fcf142dfd9f5607e1b3c284f0cfd53f07f5f1
conaryh <conaryh@gmail.com>
2018-04-23 16:26:25 -0600

    new updates

--- script.js
+++ script.js
@@ -1,65 +1,200 @@
 var json = {
     "siteName": "sandals",
     "siteCode": "sndl",
-    "cdnUrl": "https://www.cdn.com",
     "browsersBySite": {
-        "www.sandals.com": [
+        "sandals": [
             {
-                name: "Chrome",
-                minVersionSupported: 58,
-                downloadUrl: "https://www.chrome.com"
+                name: "Edge",
+                minVersionSupported: 20,
+                downloadUrl: "https://www.microsoft.com/en-us/download/details.aspx?id=48126"
             },
             {
-                name: "Safari",
+                name: "IE",
                 minVersionSupported: 10,
-                downloadUrl: "https://www.safari.com"
-            },
-        ],
-        "www.beaches.com": [
-            {
-                name: "Chrome",
-                minVersionSupported: 56,
-                downloadUrl: "https://www.chrome.com"
-            },
-            {
-                name: "Safari",
-                minVersionSupported: 9,
-                downloadUrl: "https://www.safari.com"
-            },
-        ],
-        "sandalsselect.com": [
-            {
-                name: "Chrome",
-                minVersionSupported: 58,
-                downloadUrl: "https://www.chrome.com"
-            },
-            {
-                name: "Safari",
-                minVersionSupported: 10,
-                downloadUrl: "https://www.safari.com"
+                downloadUrl: "https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx"
             },
             {
                 name: "Firefox",
+                minVersionSupported: 34,
+                downloadUrl: "https://www.mozilla.org/en-US/firefox/new/"
+            },
+            {
+                name: "Safari",
+                minVersionSupported: 70,
+                downloadUrl: "https://support.apple.com/en-us/HT204416"
+            },
+            {
+                name: "Chrome",
+                minVersionSupported: 30,
+                downloadUrl: "https://www.google.com.mx/chrome/?brand=CHBD&gclid=EAIaIQobChMIp5qpt-jQ2gIVyySBCh1JUQmLEAAYASAAEgImWvD_BwE"
+            },
+        ],
+        "beaches": [
+            {
+                name: "Edge",
+                minVersionSupported: 20,
+                downloadUrl: "https://www.microsoft.com/en-us/download/details.aspx?id=48126"
+            },
+            {
+                name: "IE",
                 minVersionSupported: 10,
-                downloadUrl: "https://www.safari.com"
+                downloadUrl: "https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx"
+            },
+            {
+                name: "Firefox",
+                minVersionSupported: 34,
+                downloadUrl: "https://www.mozilla.org/en-US/firefox/new/"
+            },
+            {
+                name: "Safari",
+                minVersionSupported: 7,
+                downloadUrl: "https://support.apple.com/en-us/HT204416"
+            },
+            {
+                name: "Chrome",
+                minVersionSupported: 30,
+                downloadUrl: "https://www.google.com.mx/chrome/?brand=CHBD&gclid=EAIaIQobChMIp5qpt-jQ2gIVyySBCh1JUQmLEAAYASAAEgImWvD_BwE"
+            },
+        ],
+        "sandalsselect": [
+            {
+                name: "Edge",
+                minVersionSupported: 20,
+                downloadUrl: "https://www.microsoft.com/en-us/download/details.aspx?id=48126"
+            },
+            {
+                name: "IE",
+                minVersionSupported: 10,
+                downloadUrl: "https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx"
+            },
+            {
+                name: "Firefox",
+                minVersionSupported: 34,
+                downloadUrl: "https://www.mozilla.org/en-US/firefox/new/"
+            },
+            {
+                name: "Safari",
+                minVersionSupported: 7,
+                downloadUrl: "https://support.apple.com/en-us/HT204416"
+            },
+            {
+                name: "Chrome",
+                minVersionSupported: 70,
+                downloadUrl: "https://www.google.com.mx/chrome/?brand=CHBD&gclid=EAIaIQobChMIp5qpt-jQ2gIVyySBCh1JUQmLEAAYASAAEgImWvD_BwE"
+            },
+        ],
+        "sandalsfoundation": [
+            {
+                name: "Edge",
+                minVersionSupported: 20,
+                downloadUrl: "https://www.microsoft.com/en-us/download/details.aspx?id=48126"
+            },
+            {
+                name: "IE",
+                minVersionSupported: 10,
+                downloadUrl: "https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx"
+            },
+            {
+                name: "Firefox",
+                minVersionSupported: 34,
+                downloadUrl: "https://www.mozilla.org/en-US/firefox/new/"
+            },
+            {
+                name: "Safari",
+                minVersionSupported: 7,
+                downloadUrl: "https://support.apple.com/en-us/HT204416"
+            },
+            {
+                name: "Chrome",
+                minVersionSupported: 70,
+                downloadUrl: "https://www.google.com.mx/chrome/?brand=CHBD&gclid=EAIaIQobChMIp5qpt-jQ2gIVyySBCh1JUQmLEAAYASAAEgImWvD_BwE"
+            },
+        ],
+        "islandroutes": [
+            {
+                name: "Edge",
+                minVersionSupported: 20,
+                downloadUrl: "https://www.microsoft.com/en-us/download/details.aspx?id=48126"
+            },
+            {
+                name: "IE",
+                minVersionSupported: 10,
+                downloadUrl: "https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx"
+            },
+            {
+                name: "Firefox",
+                minVersionSupported: 34,
+                downloadUrl: "https://www.mozilla.org/en-US/firefox/new/"
+            },
+            {
+                name: "Safari",
+                minVersionSupported: 7,
+                downloadUrl: "https://support.apple.com/en-us/HT204416"
+            },
+            {
+                name: "Chrome",
+                minVersionSupported: 30,
+                downloadUrl: "https://www.google.com.mx/chrome/?brand=CHBD&gclid=EAIaIQobChMIp5qpt-jQ2gIVyySBCh1JUQmLEAAYASAAEgImWvD_BwE"
+            },
+        ],
+        "fowlcay": [
+            {
+                name: "Edge",
+                minVersionSupported: 20,
+                downloadUrl: "https://www.microsoft.com/en-us/download/details.aspx?id=48126"
+            },
+            {
+                name: "IE",
+                minVersionSupported: 10,
+                downloadUrl: "https://www.microsoft.com/en-us/download/internet-explorer-11-for-windows-7-details.aspx"
+            },
+            {
+                name: "Firefox",
+                minVersionSupported: 34,
+                downloadUrl: "https://www.mozilla.org/en-US/firefox/new/"
+            },
+            {
+                name: "Safari",
+                minVersionSupported: 7,
+                downloadUrl: "https://support.apple.com/en-us/HT204416"
+            },
+            {
+                name: "Chrome",
+                minVersionSupported: 30,
+                downloadUrl: "https://www.google.com.mx/chrome/?brand=CHBD&gclid=EAIaIQobChMIp5qpt-jQ2gIVyySBCh1JUQmLEAAYASAAEgImWvD_BwE"
             },
         ]
     },
     "modalContent": {
-        sandals:{
+        "sandals":{
             "icon":"icon-sandals",
             "text": "This website is designed using new technology and best practices",
-            "highlightText": "We recommend upgrading your browser"
+            "highlightText": "WE RECOMMEND UPGRADING YOUR BROWSER TO PROPERLY VIEW OUR WEBSITE.",
         },
-        beaches: {
+        "beaches": {
             "icon":"icon-beaches",
             "text": "This website is designed using new technology and best practices",
-            "highlightText": "We recommend upgrading your browser"
+            "highlightText": "WE RECOMMEND UPGRADING YOUR BROWSER TO PROPERLY VIEW OUR WEBSITE.",
         },
-        ssg: {
+        "sandalsselect": {
             "icon":"icon-ssg",
             "text": "This website is designed using new technology and best practices",
-            "highlightText": "We recommend upgrading your browser"
+            "highlightText": "WE RECOMMEND UPGRADING YOUR BROWSER TO PROPERLY VIEW OUR WEBSITE.",
+        },
+        "sandalsfoundation": {
+            "icon":"icon-sandals",
+            "text": "This website is designed using new technology and best practices",
+            "highlightText": "WE RECOMMEND UPGRADING YOUR BROWSER TO PROPERLY VIEW OUR WEBSITE.",
+        },
+        "islandroutes": {
+            "icon":"icon-islandroutes-logo",
+            "text": "This website is designed using new technology and best practices",
+            "highlightText": "WE RECOMMEND UPGRADING YOUR BROWSER TO PROPERLY VIEW OUR WEBSITE.",
+        },
+        "fowlcay": {
+            "icon":"icon-fowl-cay-new",
+            "text": "This website is designed using new technology and best practices",
+            "highlightText": "WE RECOMMEND UPGRADING YOUR BROWSER TO PROPERLY VIEW OUR WEBSITE.",
         }
     },
 }
@@ -84,8 +219,26 @@ var browser = navigator.myBrowser;
 var browserName = browser.match(/[a-zA-Z]+/g);
 var browserVersion = parseInt(browser.match(/\d+/)[0]);
 var siteName = window.location.hostname !== "" ? window.location.hostname : console.log('the browser script is not working probably because you are in a local server');
+siteName = siteName.substring(0, siteName.lastIndexOf(".")).replace(/^www\./,'');
+
 var lengthBrowsers = json.browsersBySite[siteName] ? json.browsersBySite[siteName].length : 0;
 var body = document.querySelector('body');
+
+var internals = {};
+
+internals.displayOnce = function() {
+  if (document.cookie.replace(/(?:(?:^|.*;\s*)DISPLAYWARNING\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+    alert("Do something here!");
+    var date = new Date();
+    var expirationDays = 5;
+    date.setTime(date.getTime() + (expirationDays*24*60*60*1000));
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = "DISPLAYWARNING=true; " + expires;
+    addEvent("load", document, validateBrowser() );
+  }
+
+  console.log("cookie");
+}
 
 function validateBrowser() {
 
@@ -95,15 +248,21 @@ function validateBrowser() {
 
             if (browserVersion < json.browsersBySite[siteName][x].minVersionSupported) {
                 console.log("version desactualizada");
-                createModalBox();
+                createModalBox(x);
             }
         }
     }
 }
 
-function createModalBox() {
+function createModalBox(pos) {
     var modalWrapper = document.createElement('div');
     var modal = document.createElement('div');
+    var modalIcon = document.createElement('i');
+    var modalText = document.createElement('p');
+    var modalTextContent = document.createTextNode(json.modalContent[siteName].text);
+    var modalHighlight = document.createElement('a');
+    var modalHighlightText = document.createTextNode(json.modalContent[siteName].highlightText);
+
     var modalItems = json.modalContent.length;
     var pageWidth;
     var pageHeight;
@@ -143,6 +302,30 @@ function createModalBox() {
     modalWrapper.style.left = "50%";
     modalWrapper.style.marginLeft = "-" + halfMiddle + "px";
 
+
+    //modal content generated by json
+    modalIcon.classList.add(json.modalContent[siteName].icon);
+    modalIcon.style.textAlign = "center";
+    modalIcon.style.display = "block";
+    modalIcon.style.paddingTop = "50px";
+    modalIcon.style.fontSize = "45px";
+    modalIcon.style.color = "#809a00";
+
+    modalHighlight.setAttribute("href", json.browsersBySite[siteName][pos].downloadUrl );
+    modalHighlight.setAttribute("target", "_blank" );
+    modalText.style.font = "16.5px/27px arial";
+    modalText.style.textAlign = "center";
+    modalText.style.color = "#373737";
+    modalText.style.marginBottom = "4px";
+
+    modalHighlight.style.font = "20px/32px arial";
+    modalHighlight.style.textAlign = "center";
+    modalHighlight.style.color = "#809a00";
+    modalHighlight.style.marginTop = "0";
+    modalHighlight.style.fontWeight = "bold";
+    modalHighlight.style.display = "block";
+
+    //only is necesary when the modal is created
     function closeModal() {
         body.style.overflow= "scroll";
         body.removeChild(modal);
@@ -150,19 +333,32 @@ function createModalBox() {
         removeEvent("click", modal, closeModal );
     }
 
-    function auxClose(e) {
-        if (e.keyCode == 27) {
-            body.removeChild(modal);
-            body.removeChild(modalWrapper);
-            body.style.overflow= "scroll";
-            removeEvent("keydown", document, auxClose );
-        }
-    }
+    //adding chlds to the DOM
+    modalText.appendChild(modalTextContent);
+    modalHighlight.appendChild(modalHighlightText);
+
+    modalWrapper.appendChild(modalIcon);
+    modalWrapper.appendChild(modalText);
+    modalWrapper.appendChild(modalHighlight);
 
     addEvent("click", modal, closeModal );
     addEvent("keydown", document, auxClose );
 }
 
+//optional close with ESC
+function auxClose(e) {
+    var modal = document.getElementsByClassName('browser-modal')[0];
+    var modalWrapper = document.getElementsByClassName('browser-upgrade')[0];
+
+    if (e.keyCode == 27) {
+        body.removeChild(modal);
+        body.removeChild(modalWrapper);
+        body.style.overflow= "scroll";
+        removeEvent("keydown", document, auxClose );
+    }
+}
+
+// validating IE 8 events
 function addEvent(event, element, func) {
   if (element.addEventListener) {
         element.addEventListener(event, func, false);
@@ -179,25 +375,35 @@ function removeEvent(event, element, func) {
     }
 }
 
-addEvent("load", document, validateBrowser() );
-
-
+addEvent("load", document, internals.displayOnce() );
 /*estilos de modal
-background gradient
-background fallback
 opacidad  ****
 event listener o attach ****
 no scroll  *****
 cerrar  modal y poder scrolear  *****
 cerrar modal dando clic afuera o en X ****
-cerrar modal con esc
-generar text y icono
-acortar hostname a solo domain name
+cerrar modal con esc *****
+generar text y icono  ****
+acortar hostname a solo domain name ****
+
+
+background gradient
+background fallback
+font family
 arreglar json
 */
 
 /*falta
 agregar cookie
 agregar request de json
+considerar si otro developer quiere agregar mas hijos
+los estilos personalizados deben ser propios de cada sitio
 */
 
+
+// function generateContent() {
+//     var element = document.createElement(json.type);
+//     var elementChild = document.createElement(json.typetext);
+
+
+// }
